8c7edc0bd1b1a3a92b64a13a6b68e02e47d440e4
puldyastin <puldyastin@gmail.com>
2022-02-10 09:45:40 +0800

    loop countries and its details

--- src/components/Countries/index.vue
+++ src/components/Countries/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <div v-for="country in countries">
-      <img :src="country.flags.png" alt="" />
+  <div class="flex">
+    <div v-for="country in countries" class="card">
+      <div class="flags-container">
+        <img :src="country.flags.png" alt="" />
+      </div>
+      <div class="card-container">
+        <h3>{{ country.name.common }}</h3>
+        <div class="c">
+          <p>
+            <span>Population:</span> {{ country.population.toLocaleString() }}
+          </p>
+          <p><span>Region:</span> {{ country.region }}</p>
+          <p><span>Capital:</span> {{ country.capital }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,9 +31,36 @@ export default {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((res) => (this.countries = res.data));
-    // .then((res) => console.log(res.data[1].flags.png));
+    console.log(this.countries);
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+h3 {
+  padding-bottom: 13px;
+}
+span {
+  font-weight: 600;
+}
+.card {
+  border-radius: 8px;
+}
+.c-info {
+  padding: 0 23px 23px 23px;
+}
+.card-container {
+  padding: 16px 25px 25px 25px;
+}
+.flex {
+  flex-wrap: wrap;
+  row-gap: 30px;
+}
+/* .flags-container {
+} */
+.flags-container img {
+  width: 250px;
+  height: 170px;
+  border-radius: 8px 8px 0 0;
+}
+</style>
